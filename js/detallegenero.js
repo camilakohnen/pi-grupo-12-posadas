3467e93c987ff648d690dea5da1ce6e61a369441
camilakohnen <ckohnen@udesa.edu.ar>
2023-11-17 08:46:25 -0600

    test

--- js/detallegenero.js
+++ js/detallegenero.js
@@ -3,8 +3,8 @@ let queryString = location.search;
 let sToObj = new URLSearchParams(queryString);
 let idgenero = sToObj.get('idgenero');
 let name = sToObj.get('name'); 
-
 let urldgp = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${idgenero}`
+let urldgs  = `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_genres=${idgenero}`
 
 fetch(urldgp)
     .then(function(response){
@@ -15,19 +15,50 @@ fetch(urldgp)
         console.log(data);
         let contenido = "";
         for (let index = 0; index < 5; index++) {
+            if (info[i].poster_path!=null){
             console.log(data.results[index].poster_path)
-           contenido = `
+            contenido = `
            
-           <article class="articledetallegenero">
+            <article class="articledetallegenero">
                 <a href="./detallepelicula.html?id=${data.results[index].id}"">
+                    
                     <img class="imgdetallegenero" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="img">
     
+                </a>
+            </article>   `   
+        }};
+
+        let detallegenero = document.querySelector(".sectiondetallegenero");
+        detallegenero.innerHTML += contenido;      
+
+    })
+
+    .catch(function(error){
+        console.log(error);
+    }) 
+
+    fetch(urldgs)
+    .then(function(response){
+        return response.json()
+    })
+
+    .then(function(data){
+        console.log(data);
+        let contenidoseries = "";
+        for (let index = 0; index < 5; index++) {
+            console.log(data.results[index].poster_path)
+           contenidoseries = `
+           
+           <article class="articledetallegeneroserie">
+                <a href="./detallepelicula.html?id=${data.results[index].id}"">
+                    <img class="imgdetallegenero" src="https://image.tmdb.org/t/p/w500/${data.results[index].poster_path}" alt="img">
+                    
                 </a>
            </article>   `   
         };
 
-        let detallegenero = document.querySelector(".sectiondetallegenero");
-        detallegenero.innerHTML += contenido;      
+        let detallegeneroserie = document.querySelector(".sectiondetallegeneroseries");
+        detallegeneroserie.innerHTML += contenidoseries;      
 
     })
 
